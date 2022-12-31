b0c4e62f17d5f4ff84c343fde47b1da8bb591bb4
frank <frankcool321@gmail.com>
2022-12-31 12:27:27 +0800

    feat: add users module and login page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router(); //- express 路由器
 //- 引入路由modules
 const home = require("./modules/home");
 const todos = require("./modules/todos");
+const users = require("./modules/users")
 router.use("/", home);
 router.use("/todos", todos);
+router.use("/users", users);
 //- 匯出路由器
 module.exports = router;
---
+++ routes/modules/users.js
@@ -0,0 +1,12 @@
+//- set users route
+const router = require("express").Router();
+
+router.get("/login", (req, res) => {
+  return res.render("login");
+});
+
+router.get("/register", (req, res) => {
+  return res.render("register");
+});
+
+module.exports = router;
