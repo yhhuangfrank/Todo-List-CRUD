20ec44a7aaca25912a594492bbdcd1a1128a3de7
frank <frankcool321@gmail.com>
2023-01-01 18:04:16 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,18 +1,48 @@
-//- 載入先前設定的Todo models
+//- require related models
 const Todo = require("../todo");
+const User = require("../user");
+//- require related module
+const bcrypt = require("bcryptjs");
 
 //- 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 //! 設定資料庫連線 (並將exports出來的db接取)
-const db = require("../../config/mongoose")
+const db = require("../../config/mongoose");
+
+//- 設定SEED_USER資料
+const SEED_USER = {
+  name: "root",
+  email: "root@example.com",
+  password: "12345678",
+};
 
 //* 連線成功
 db.once("open", () => {
   //- 連線成功後發送新增資料要求,新增10筆資料
-  for (let i = 0; i < 10; i += 1) {
-    Todo.create({ name: `name-${i}` });
-  }
-  console.log("done");
+  return bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      //- 利用Array.from建立空陣列並填入Promise.all中
+      return Promise.all(
+        Array.from({ length: 10 }, (value, index) =>
+          Todo.create({ name: `name-${index}`, userId })
+        )
+      );
+    })
+    .then(() => {
+      console.log("done");
+      process.exit(); //- 結束todoSeeder
+    })
+    .catch((err) => console.log(err));
 });
